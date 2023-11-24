c670eaf1d40ecc432de4aa54872532f9ba72d151
seon3217 <seonbeatboxer23@gmail.com>
2023-11-24 15:56:46 +0900

    still bug

--- js/content.js
+++ js/content.js
@@ -1,6 +1,9 @@
 console.debug("This site is available.");
+document.body.style.backgroundColor = "red"; // debug
 
+// 受信する時はどちらもruntime
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.debug(`Received message : ${request.greeting}`);
-    sendResponse({response: "Response from background script"});
+    setTimeout(()=>sendResponse({response: "Response from background script"}), 1000);
+    return true;
   });
--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,35 @@
+async function getCurrentTab() {
+  console.debug("start getting tab");
+	let queryOptions = { active: true, currentWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+}
+
 $('#run-btn').on('click', function (e) {
 
     console.debug("clicked");
 
+    let ctab_id = 0;
+    getCurrentTab().then((tab) => {
+      console.debug(`ID: ${tab.id} title: ${tab.title}`);
+      ctab_id = tab.id;
+    }).catch((error)=>{
+      console.debug(error);
+    });
+
     // chrome.runtime.sendMessageでruntime宛てにメッセージを送信
-    chrome.runtime.sendMessage({
-      greeting: "Greeting from the content script"
-    }).then(
-      function (message) { // レスポンス受信時
-        // messageは 受信側で sendResponse関数に渡した引数が返ってきます
-        console.debug(`Received response:  ${message.response}`);
-      },
-      function (error) { // エラー時
-        console.debug(`Error: ${error}`);
-      }
-    )
+    let rtnPromise = chrome.tabs.sendMessage(ctab_id, {greeting: "Greeting from the content script"});
+    rtnPromise
+      .then(
+        function (message) { // レスポンス受信時
+          // messageは 受信側で sendResponse関数に渡した引数が返ってきます
+          console.debug(`Received response:  ${message.response}`);
+        }
+      )
+      .catch(
+        function (error) { // エラー時
+          console.debug(`Error: ${error}`);
+        }
+      )
+    
   });
